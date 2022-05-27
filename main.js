de125776e49cb99b90ef60e2ec2651599936c535
Joanna <joannaperdomo27@gmail.com>
2022-05-27 00:30:43 -0500

    trabajando en la lógica

--- main.js
+++ main.js
@@ -2,21 +2,94 @@
 const comensal = document.getElementById('comensal');
 const botonAñadirComensal = document.getElementById('boton-añadir-comensal');
 const listadeComensales = document.getElementById('lista-de-comensales')
+const comprador = document.getElementById('compradores');
+let listaDePersonas = [];
+let deudaTotal = 0;
+let todosLosComensales = document.querySelectorAll('.opcion-comprador');
 
-// Elementos para añadir productos
-const listaDeCompradores = document.getElementById('producto-comprador');
+function Persona (nombre,deuda){
+    this.nombre = nombre;
+    this.deuda = deuda;
+    this.productos = [];
+    this.numero = listaDePersonas.length+1;
+}
+
+function producto (descripcion,precio,cantidad){
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.cantidad = cantidad;
+}
 
 
-// Array con total de comensales
+// total de la cuenta
+const total = document.getElementById('total');
+
+// Array con total de comensales 
 const arrayListaDeComensales = [];
 
 // Listener para añadir comensales
-botonAñadirComensal.addEventListener('click', () => {
-    nombre = comensal.value;
-    arrayListaDeComensales.push(nombre);
-    listadeComensales.innerHTML += `<li class="list-group-item">${nombre}</li>`;
-    listaDeCompradores.innerHTML += `<option value="1">${nombre}</option>`
+botonAñadirComensal.addEventListener('click', () => {    
+    listaDePersonas.push(new Persona(comensal.value, 0));
+    listadeComensales.innerHTML += `<li id="${comensal.value}" class="list-group-item opcion-comprador">${comensal.value} $0</li>`;
+    comprador.innerHTML += `
+    <input type="checkbox" class="checkbox-comprador" id="${comensal.value}">
+    <label for="${comensal.value}">${comensal.value}</label>`
     comensal.value = "";
 })
 
+
+// Elementos para añadir productos
+let descripcion = document.getElementById('producto-descripcion');
+let precio = document.getElementById('producto-precio');
+let cantidad = document.getElementById('producto-cantidad');
+const botonAñadirProducto = document.getElementById('boton-añadir-producto');
+
+
+
+function actualizarPersona (persona){
+    const todosLosComensales = document.querySelectorAll('.opcion-comprador');
+    todosLosComensales.forEach(e => {
+        if (e.id == persona.nombre){
+            e.innerText = `${persona.nombre} $${persona.deuda}`;
+        }
+    })
+    // Actualizar total
+    total.innerText = `$${deudaTotal}`;
+}
+
 // Añadir un producto a la cuenta
+
+
+botonAñadirProducto.addEventListener('click', () =>{
+    const listadeCompradores = document.querySelectorAll('.checkbox-comprador');
+    let descripcionProducto = descripcion.value;
+    let precioProducto = parseInt(precio.value);
+    let cantidadProducto = cantidad.value;
+    listadeCompradores.forEach(e => {
+        // encontrar a las personas con check
+        if (e.checked){
+            let persona = listaDePersonas.find(elemento => elemento.nombre == e.id);
+            // actualizar deuda y productos consumidos
+            persona.deuda += precioProducto * cantidadProducto;
+            persona.productos.push(new producto(descripcionProducto,precioProducto,cantidadProducto));
+            deudaTotal += precioProducto * cantidadProducto;
+            // actualizar HTML
+            actualizarPersona(persona);
+        }
+    });
+})
+
+const botonWhatsapp = document.getElementById('boton-enviar-whatsapp');
+
+botonWhatsapp.addEventListener('click', () =>{
+    let linkWhatsapp = "https://api.whatsapp.com/send/?text=%F0%9F%92%B5%20Tu%20cuenta%20se%20divide%20as%C3%AD%3A%0A";
+    for (let persona of listaDePersonas){
+    linkWhatsapp += `${persona.nombre}:%20$${persona.deuda}%0A`;
+    }
+    linkWhatsapp += `Total%20a%20pagar:%20$${deudaTotal}`
+    window.location.href = linkWhatsapp;
+})
+
+
+
+
