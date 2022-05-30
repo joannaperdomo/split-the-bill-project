5e3443c21655bac463b36b765a28bb5a5d203a4d
Joanna <joannaperdomo27@gmail.com>
2022-05-30 01:09:07 -0500

    resolver el problema de los checkboxes

--- main.js
+++ main.js
@@ -28,12 +28,14 @@ const arrayListaDeComensales = [];
 // Listener para añadir personas
 botonAñadirComensal.addEventListener('click', () => {    
     listaDePersonas.push(new Persona(comensal.value, 0));
-    listadeComensales.innerHTML += `<li id="${comensal.value}" class="list-group-item opcion-comprador">${comensal.value} $0</li>`;
+    listadeComensales.innerHTML += `<li id="${comensal.value}-li" class="list-group-item opcion-comprador">${comensal.value}: $0</li>`;
     comprador.innerHTML += `
-    <input name="chckbox" type="checkbox" class="checkbox-comprador" id="${comensal.value}">
-    <label class="checkbox-label" for="${comensal.value}">${comensal.value}</label>`
+    <input name="chckbox" type="checkbox" class="btn-check form-check-input checkbox-comprador" id="${comensal.value}">
+    <label class="form-check-label btn btn-outline-primary checkbox-label" for="${comensal.value}">${comensal.value}</label>`
     comensal.value = "";
     total.style.visibility = 'hidden'&& (total.style.visibility = 'visible');
+    compartidoContainer.style.visibility = 'hidden'&& (compartidoContainer.style.visibility = 'visible');
+
 })
 
 
@@ -43,14 +45,15 @@ let precio = document.getElementById('producto-precio');
 let cantidad = document.getElementById('producto-cantidad');
 const botonAñadirProducto = document.getElementById('boton-añadir-producto');
 let compartido = document.getElementById('compartido');
+const compartidoContainer = document.getElementById('compartido-container');
 
 // Actualizar la deuda de la persona
 
 function actualizarPersona (persona){
     const todosLosComensales = document.querySelectorAll('.opcion-comprador');
     todosLosComensales.forEach(e => {
-        if (e.id == persona.nombre){
-            e.innerText = `${persona.nombre} $${persona.deuda}`;
+        if (e.id == `${persona.nombre}-li`){
+            e.innerText = `${persona.nombre}: $${persona.deuda}`;
         }
     })
     // Actualizar total
@@ -83,7 +86,7 @@ const botonWhatsapp = document.getElementById('boton-enviar-whatsapp');
 botonWhatsapp.addEventListener('click', () =>{
     let linkWhatsapp = "https://api.whatsapp.com/send/?text=%F0%9F%92%B5%20Tu%20cuenta%20se%20divide%20as%C3%AD%3A%0A";
     for (let persona of listaDePersonas){
-    linkWhatsapp += `•${persona.nombre}:%20$${persona.deuda}%0A`;
+    linkWhatsapp += `•%20${persona.nombre}:%20$${persona.deuda}%0A`;
     }
     linkWhatsapp += `Total%20a%20pagar:%20$${deudaTotal}`
     window.location.href = linkWhatsapp;
@@ -108,6 +111,12 @@ botonBorrar.onclick = () => {
     listadeComensales.innerHTML = "";
     listaDePersonas = "";
     total.innerText = "$0";
+    comprador.innerHTML = "";
+    total.style.visibility = "hidden";
+    compartidoContainer.style.visibility = "hidden";
+    cantidad.value = "";
+    descripcion.value = "";
+    precio.value = "";
 }
 
 
